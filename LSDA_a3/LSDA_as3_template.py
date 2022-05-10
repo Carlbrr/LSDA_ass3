314454154bf9905f7450aae310b1599465409804
Carlbrr <90042145+Carlbrr@users.noreply.github.com>
2022-05-10 12:15:34 +0200

    Update LSDA_as3_template.py

--- LSDA_a3/LSDA_as3_template.py
+++ LSDA_a3/LSDA_as3_template.py
@@ -6,16 +6,16 @@ from azureml.core import Workspace
 ##ws = Workspace.from_config()
 ws = Workspace(subscription_id = "aabeddb0-41f5-4bcc-85e9-94af5d2928f5", resource_group = "myVM_group", workspace_name = "ML-ws", auth=None, _location=None, _disable_service_check=False, _workspace_id=None, sku='basic', tags=None, _cloud='AzureCloud')
 print(ws)
-#mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri()) #uncomment
+mlflow.set_tracking_uri("azureml://japaneast.api.azureml.ms/mlflow/v1.0/subscriptions/aabeddb0-41f5-4bcc-85e9-94af5d2928f5/resourceGroups/myVM_group/providers/Microsoft.MachineLearningServices/workspaces/ML-ws") #uncomment
 
 print(ws.get_mlflow_tracking_uri())
 
 ## NOTE: Optionally, you can use the public tracking server.  Do not use it for data you cannot afford to lose. See note in assignment text. If you leave this line as a comment, mlflow will save the runs to your local filesystem.
 
-mlflow.set_tracking_uri("https://training.itu.dk:5000/")
+##mlflow.set_tracking_uri("https://training.itu.dk:5000/")
 
 # TODO: Set the experiment name
-#mlflow.set_experiment("first")
+mlflow.set_experiment("first")
 
 # Import some of the sklearn modules you are likely to use.
 from sklearn.pipeline import Pipeline
